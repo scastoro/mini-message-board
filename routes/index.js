7979229d6481bd96c412ebc56cf5d8fb30c749b4
Sal Castoro <20549092+scastoro@users.noreply.github.com>
2022-03-18 13:26:27 -0400

    Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+  {
+    text: "Greetings Globe!",
+    user: "Steve",
+    added: new Date(),
+  },
+  {
+    text: "What's up?",
+    user: "Bill",
+    added: new Date(),
+  },
+];
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini MessageBoard", messages: messages });
+});
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Form" });
+});
+router.post("/new", function (req, res, next) {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.messageUser;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
